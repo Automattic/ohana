d09373318a0a70dfda06d20876f6b4d4592e446a
laurelfulford <laurelfulford@gmail.com>
2017-12-14 15:36:02 -1000

    Building out theme styles.

--- single.php
+++ single.php
@@ -17,7 +17,10 @@ get_header(); ?>
 
 			get_template_part( 'template-parts/content', get_post_type() );
 
-			the_post_navigation();
+			the_post_navigation( array(
+				'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'ohana' ) . '</span> <span class="nav-title">%title</span>',
+				'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'ohana' ) . '</span> <span class="nav-title">%title</span>',
+			) );
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
