8f37316febaf6b169eca5d0f0fa63852d1f80514
Laurel Fulford <laurelfulford@gmail.com>
2018-01-18 12:47:25 -0800

    Fixing comment avatar size.

--- comments.php
+++ comments.php
@@ -50,8 +50,9 @@ if ( post_password_required() ) {
 		<ol class="comment-list">
 			<?php
 				wp_list_comments( array(
-					'style'      => 'ol',
-					'short_ping' => true,
+					'style'       => 'ol',
+					'short_ping'  => true,
+					'avatar_size' => 70,
 				) );
 			?>
 		</ol><!-- .comment-list -->
